915da1165dec1605bffa1449b764825ec4b9f11e
marik <gaurav.marik@gmail.com>
2022-12-18 16:29:57 -0500

    components, context, routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import { ThemeProvider } from './context/ThemeContext'
+import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './routes/Home'
+import Signin from './routes/Signin'
+import Signup from './routes/Signup'
+import Account from './routes/Account'
+import axios from 'axios'
+import CoinPage from './routes/CoinPage'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [coins, setCoins] = useState([])
+
+	const url =
+		'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=30&page=1&sparkline=true'
+
+	useEffect(() => {
+		axios.get(url).then((response) => {
+			setCoins(response.data)
+			// console.log(response.data)
+		})
+	}, [url])
+
+	return (
+		<ThemeProvider>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home coins={coins} />} />
+				<Route path="/signin" element={<Signin />} />
+				<Route path="/signup" element={<Signup />} />
+				<Route path="/account" element={<Account />} />
+				<Route path="coin/:coinId" element={<CoinPage />}>
+					<Route path=":coinId" />
+				</Route>
+			</Routes>
+		</ThemeProvider>
+	)
 }
 
-export default App;
+export default App
---
+++ src/routes/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CoinSearch from '../components/CoinSearch'
+import Trending from '../components/Trending'
+
+const Home = ({ coins }) => {
+	return (
+		<div>
+			<CoinSearch coins={coins} />
+			<Trending />
+		</div>
+	)
+}
+export default Home
