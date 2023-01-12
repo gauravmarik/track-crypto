a8d5c22f532d0416c9e03a54e9f117a566ccb979
marik <gaurav.marik@gmail.com>
2023-01-11 21:34:47 -0500

    ThemeContext, Account, CoinPage

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import CoinSearch from '../components/CoinSearch'
-// import Trending from '../components/Trending'
+import Trending from '../components/Trending'
 
 const Home = ({ coins }) => {
 	return (
 		<div>
 			<CoinSearch coins={coins} />
-			{/* <Trending /> */}
+			<Trending />
 		</div>
 	)
 }
